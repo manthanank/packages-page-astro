f933cbc4138f0cc21e8153c7ebfc6eec9896bcec
Manthan Ankolekar <manthan.ank@gmail.com>
2022-12-12 13:29:47 +0530

    changes made

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
   site: "https://astro-theme-creek.netlify.app/",
+  output: "server",
+  adapter: netlify()
 });
